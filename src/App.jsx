afd11141658874bb8a32456483c01dc4b7022c6e
Megha Joshi <tup77688@temple.edu>
2025-03-20 19:04:33 -0400

    React and Vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>SmartSchedule</h1>
+      <p>Temple University's new schedule creator.</p>
+    </>
+
+  
+  )
+}
+
+export default App
